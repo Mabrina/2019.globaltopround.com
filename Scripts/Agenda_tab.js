eed295cef56c0975e61819d9e67ad790e15058a4
mabrina <jsatbc@gmail.com>
2019-05-15 21:21:29 +0900

    first commit

---
+++ Scripts/Agenda_tab.js
@@ -0,0 +1,26 @@
+﻿
+/* agenda day 버튼 호환 */
+
+$(document).ready(function () {
+    //기본 설정값
+    $(".Agenda_01").show();
+    $(".Agenda_02").hide();
+    $('.tba_01').css({ 'color': '#fff' });
+
+    $(".tba_01").click(function () {
+        //누르면 초기화
+        $(".Agenda_01").show();
+        $(".Agenda_02").hide();
+        $('.tba_01').css({ 'color': '#fff'});
+        $('.tba_02').css({ 'color': '#909396', 'background': 'none' });
+        return false;
+    });
+    $(".tba_02").click(function () {
+        //누르면 초기화
+        $(".Agenda_02").show();
+        $(".Agenda_01").hide();
+        $('.tba_02').css({ 'color': '#fff'});
+        $('.tba_01').css({ 'color': '#909396', 'background': 'none' });
+        return false;
+    });
+});
